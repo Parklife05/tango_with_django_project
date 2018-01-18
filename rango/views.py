1e7130d26acf3978bba4095a213faf84f189f9b4
james park <2271862p@student.gla.ac.uk>
2018-01-18 23:34:13 +0000

    chapter 4 last commit not displaying media

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
@@ -8,4 +9,4 @@ def index(request):
    
 
 def about(request): 
-    return HttpResponse("Rango says here is the about page. <br/>  <a href='/rango/'>Index</a>") 
+    return render(request, 'rango/index.html') 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 print(__file__)
@@ -70,6 +71,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+
             ],
         },
     },
@@ -121,6 +124,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# media files
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
