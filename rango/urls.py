a9f8e152a585d136874b04ba556fbfe0f9e55529
james park <2271862p@student.gla.ac.uk>
2018-01-22 21:42:44 +0000

    chapter 4 commit for test purposes

--- rango/urls.py
+++ rango/urls.py
@@ -4,8 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url (r'^about/$',views.about, name = 'about')
-
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^index/$', views.index, name='Index'),
+    url (r'^about/$',views.about, name = 'About')] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
