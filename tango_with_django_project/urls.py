6890f0fdb90f34942ec31fe649837f90ca31bac8
james park <2271862p@student.gla.ac.uk>
2018-01-24 18:25:57 +0000

    first commit

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,13 +17,17 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include 
 from rango import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^$', views.index, name='Index'),
-    url (r'^about/$',views.about, name = 'About'),
     url(r'^rango/', include('rango.urls')),
+    url(r'^about/', views.about, name='About' ),
+
     # above maps any URLs starting
     # with rango/ to be handled by
     # the rango application 
     url(r'^admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
