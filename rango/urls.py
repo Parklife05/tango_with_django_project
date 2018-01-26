7003ac54429191e3ab7b255f8dea246903bd9bb1
James Park <2271862p@student.gla.ac.uk>
2018-01-26 15:16:59 +0000

    chapter 6 complete

--- rango/urls.py
+++ rango/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
-    url (r'^about/$',views.about, name = 'about')]
+    url(r'^$', views.index, name='index'),
+    url (r'^about/$',views.about, name = 'about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category, name='show_category'),
+    ]
 
 
 
